c027c6284998dcd8a04fbc78869c053bc4f9b6a5
bostonkassidy <47906726+bostonkassidy@users.noreply.github.com>
2023-02-23 08:31:26 -0600

    Update and rename CapacitiveSensor_41_UDP_and_MIDI.ino to CapacitiveSensor_4_MIDI.ino

--- CapacitiveSensor_4_MIDI.ino
+++ CapacitiveSensor_4_MIDI.ino
@@ -7,27 +7,10 @@
  * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
  */
 #include <elapsedMillis.h>
-#include <NativeEthernet.h>
-#include <NativeEthernetUdp.h>
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-
-IPAddress ip(192, 168, 43, 177);
-IPAddress pc(192, 168, 43, 43);
-
-unsigned int localPort = 8888;      // local port to listen on
-
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
 
 const int sensorCount = 16;
 int touchFlag[sensorCount];
 long total[sensorCount];
-int symbolNum[] = {49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64};
 
 long threshhold = 1500;
 long upperThreshhold = threshhold+1000;
@@ -92,23 +75,6 @@ void setup()
 {
    Serial.begin(9600);
    pinMode(led, OUTPUT);
-   Ethernet.begin(mac, ip);
-/*
-   // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-*/
-
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
-  // start UDP
-  Udp.begin(localPort);   
 
    for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings1[thisReading] = 0;
@@ -252,11 +218,6 @@ for(int i = 0; i <= sensorCount-1; i++){
   readSensor(16, 75);
   delay(50);
 
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.print("resetting");
-    resetFunc();
-  }
-
   while (usbMIDI.read()) {
     // ignore incoming messages
   }
@@ -304,12 +265,6 @@ void readSensor(int sensorNumber, int midiNote){
          touchFlag[i] = 0;
         }
       touchFlag[sensorNumber-1] = 1;
-
-      ///Send UDP
-      Udp.beginPacket(pc, 8888);
-      Udp.write(symbolNum[sensorNumber-1]);
-      //Udp.write(average[sensorNumber-1]);
-      Udp.endPacket();
 
       ///Send MIDI ON
       usbMIDI.sendNoteOn(midiNote, 127, channel);  // 60 = C4
